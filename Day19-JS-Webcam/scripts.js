f1432529c9bf509d37f1127bd6653e6a4c90f6f5
Arin M <arinmsn@gmail.com>
2019-08-25 04:40:13 -0700

    rgbSplit and greenScreen effects

--- Day19-JS-Webcam/scripts.js
+++ Day19-JS-Webcam/scripts.js
@@ -25,8 +25,18 @@ function paintToCanvas() {
 
 	return setInterval(() => {
 		ctx.drawImage(video, 0, 0, width, height);
+		// Take the pixels out
 		let pixels = ctx.getImageDate(0, 0, width, height);
+		// Apply the video effect filters
 		pixels = redEffect(pixels);
+
+		// pixels = rgbSplit(pixels);
+		// ctx.globalAlpha = 0.8;
+
+		// pixels = greenScreen(pixels);
+		// Put back the pixels
+
+		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
 
@@ -44,7 +54,44 @@ function takePhoto() {
 	strip.insertBefore(link, strip.firstChild);
 }
 
-//  Filters
+// Video Effect Filters
+function rgbSplit(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i - 150] = pixels.data[i + 0]; // red
+		pixels.data[i + 500] = pixels.data[i + 1]; // green
+		pixels.data[i - 550] = pixels.data[i + 2]; // blue
+	}
+	return pixels;
+}
+
+function greenScreen(pixels) {
+	const levels = {};
+
+	document.querySelectorAll(".rgb input").forEach(input => {
+		levels[input.name] = input.value;
+	});
+
+	for (i = 0; i < pixels.data.length; i = i + 4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 3];
+
+		if (
+			red >= levels.rmin &&
+			green >= levels.gmin &&
+			blue >= levels.bmin &&
+			red <= levels.rmax &&
+			green <= levels.gmax &&
+			blue <= levels.bmax
+		) {
+			// take it out!
+			pixels.data[i + 3] = 0;
+		}
+	}
+
+	return pixels;
+}
 
 getVideo();
 
