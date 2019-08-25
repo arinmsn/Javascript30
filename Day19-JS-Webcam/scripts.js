3d7304692127b3db17a3eaff49cadf75755623de
Arin M <arinmsn@gmail.com>
2019-08-25 04:19:23 -0700

    Webcam working, app taking pictures, current webcam is not working. Will test it on another machine.

--- Day19-JS-Webcam/scripts.js
+++ Day19-JS-Webcam/scripts.js
@@ -11,6 +11,41 @@ function getVideo() {
 			console.log(localMediaStream);
 			video.src = window.URL.createObjectURL(localMediaStream);
 			video.play();
+		})
+		.catch(err => {
+			console.error(`Error: `, err);
 		});
 }
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		let pixels = ctx.getImageDate(0, 0, width, height);
+		pixels = redEffect(pixels);
+	}, 16);
+}
+
+function takePhoto() {
+	// Play the sound
+	snap.currentTime = 0;
+	snap.play();
+
+	// Take data out of the canvas
+	const data = canvas.toDataURL("image/jpeg");
+	const link = document.createElement("a");
+	link.href = data;
+	link.setAttribute("download", "webcam-Photo");
+	link.innerHTML = `<img src="${data}" alt="snapshot of webcam" />`;
+	strip.insertBefore(link, strip.firstChild);
+}
+
+//  Filters
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
