380e1155eb19accdd3fb863c7699762b56f3abac
arinmsn <arinmsn@gmail.com>
2019-10-04 21:37:54 -0700

    Day 29 - Countdown Timer - Finished

---
+++ Day29-Countdown-Timer/scripts.js
@@ -0,0 +1,66 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
+
+function timer(seconds) {
+  // Clear any existing timers
+  clearInterval(countdown);
+
+  // When counter started
+  // const now = (New Date()).getTime();
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeleft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    // Check if we shuld stop
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    // Display it
+    displayTimeleft(secondsLeft);
+  }, 1000);
+}
+
+// So that timer starts counting down immediately
+// Without using this F(n), interval waits a second then starts
+function displayTimeleft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+// Showing ending time
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${adjustedHour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener("click", startTimer));
+
+document.customForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
